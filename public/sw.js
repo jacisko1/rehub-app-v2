1ffdd8948cc2bccda4c313b1acb37655aaafdb3a
jacisko1 <jakub.jacisko@gmail.com>
2026-02-23 22:00:46 +0100

    Update Android/PWA icons to new app logo and bust cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "rehub-v2";
-const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg", "/icon-192.png", "/icon-512.png"];
+const CACHE_NAME = "rehub-v3";
+const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg", "/icon-192-v2.png", "/icon-512-v2.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
